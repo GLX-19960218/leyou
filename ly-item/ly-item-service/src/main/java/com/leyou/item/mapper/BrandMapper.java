e04495b2a1a9677175c873a9d06c68c0b1ce3c0b
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2018-10-31 17:26:46 +0800

    品牌服务

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,14 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Title: BrandMapper.java
+ * @Package com.leyou.item.mapper
+ * @Description: TODO
+ * @Author ZhangYB
+ * @Version V1.0
+ */
+public interface BrandMapper extends Mapper<Brand> {
+}
